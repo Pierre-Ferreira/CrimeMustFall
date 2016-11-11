9bc947e33d1233f94dd109721518fd6ea5ed2830
Pierre Ferreira <pierre@tektite.biz>
2016-11-11 20:56:52 +0200

    Emergency Contacts connect request Part 2.

--- server/methods/create_contact_connect_request.js
+++ server/methods/create_contact_connect_request.js
@@ -5,11 +5,13 @@ import {check} from 'meteor/check';
 export default function () {
   Meteor.methods({
     'create_contact_connect_request'(contactID) {
+      check(contactID, String)
       console.log('contactID:',contactID)
       const userID = Meteor.userId()
       //Update the contact being requested.
-      Meteor.users.update(contactID,{ $push: { "profile.contacts2.requestsToMe" : userID } })
+      Meteor.users.update(contactID,{ $push: { "profile.contacts.requestsToMe" : userID } })
       //Update the user with the request sent.
+      Meteor.users.update(userID,{ $push: { "profile.contacts.requestedByMe" : contactID } })
     }
   });
 }
