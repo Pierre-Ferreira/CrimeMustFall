4480891082ee4ef6027295e4d7b687d1086aa53b
Pierre Ferreira <pierre@tektite.biz>
2016-11-11 21:56:24 +0200

    Emergency Contacts connect request Part 3.

--- client/modules/core/actions/emergency_contacts_search.js
+++ client/modules/core/actions/emergency_contacts_search.js
@@ -7,11 +7,25 @@ export default {
       } else {
         let objArr = []
         let uniqueArr = []
+        // Build a single array from the multi array.
         for (let x = 0; x < result.length; x++) {
           for(let y = 0; y < result[x].length; y++) {
-            objArr.push(result[x][y])
+            // Check if the contact is connected to the user.
+            // Check if the contact has a connect request from the user.
+            let requestsToMeArr = result[x][y].profile.contacts.requestsToMe
+            for(let z = 0; z < requestsToMeArr.length; z++) {
+              if (requestsToMeArr[z] === Meteor.userId()) {
+                result[x][y].connectRequest = true
+              } else {
+                result[x][y].connectRequest = false                
+              }
+            }
+            // Leave out the current user. Do not display it.
+            if(result[x][y]._id !== Meteor.userId())
+              objArr.push(result[x][y])
           }
         }
+        // Remove duplicates.
         uniqueArr = _.uniq(objArr, (x) => {
           return x._id
         })
